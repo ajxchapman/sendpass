fed40d92d5648a3f7a7cea895a574543cace0578
Alex Chapman <3729326+ajxchapman@users.noreply.github.com>
2020-11-17 15:57:07 +0000

    Initial attempt

--- src/index.js
+++ src/index.js
@@ -38,18 +38,18 @@ function escapeHTML(text) {
 
 function generateQR() {
   const recv_settings = {
-    url : location.href,
     uuid : uuidv4(),
     key : CryptoJS.lib.WordArray.random(128 / 8).toString(),
     iv: CryptoJS.lib.WordArray.random(128 / 8).toString()
   };
+  const qr_data = `${location.protocol}//${location.host}/#${JSON.stringify(recv_settings)}`;
 
-  log('Recv settings', {recv_settings});
+  log('Recv settings', {qr_data});
 
   let image = new Image();
   image.src = '/assets/qr_logo.png';
   image.onload = function() {
-    QRCode.toCanvas(canvas, JSON.stringify(recv_settings), {margin: 2, width: 480}, () => {
+    QRCode.toCanvas(canvas, qr_data, {margin: 2, width: 480}, () => {
       let context = canvas.getContext('2d');
       
       context.drawImage(image, 
@@ -81,7 +81,7 @@ function scanQR(callback) {
               QRScanner.scanImage(video, null, engine).then(result => {
                 log("scanQR", {result});
 
-                callback(JSON.parse(result));
+                callback(result);
 
                 // Stop and kill the video stream
                 video.srcObject.getTracks().forEach(function(track) { track.stop(); });
@@ -112,15 +112,29 @@ function wsSend(send_settings, payload) {
   var sendSocket = new WebSocket(`wss://${location.host}/send/${send_settings.uuid}`);
   sendSocket.onopen = () => {
     let data = {
-      uuid : send_settings.uuid, 
-      url : send_settings.url, 
-      // Encrypt payload
-      payload: AES.encrypt(JSON.stringify(payload), send_settings.key, { iv: send_settings.iv }).toString()
+      uuid : send_settings.uuid,
+      payload : payload
     };
+
+    // Encrypt payload
+    if (send_settings.key != null && send_settings.iv != null) {
+      data.payload = AES.encrypt(data.payload, send_settings.key, { iv: send_settings.iv }).toString()
+    }
     log("wsSend", {data});
 
     sendSocket.send(JSON.stringify(data));
   };
+}
+
+function redirect(url, payload) {
+  let send_settings = JSON.parse(decodeURIComponent(url.hash.slice(1)))
+  
+  url.hash = JSON.stringify({
+    uuid : send_settings.uuid,
+    // Encrypt payload
+    payload : AES.encrypt(JSON.stringify(payload), send_settings.key, { iv: send_settings.iv }).toString()
+  });
+  location = url;
 }
 
 function wsRecv(recv_settings, callback) {
@@ -131,56 +145,101 @@ function wsRecv(recv_settings, callback) {
 
     // Decrypt payload
     data.payload = JSON.parse(AES.decrypt(data.payload, recv_settings.key, { iv : recv_settings.iv }).toString(CryptoJS.enc.Utf8));
-    callback(data);
+    callback(data.payload);
   };
 }
 
-function recvMessage() {
+function recvMessage(callback) {
   const recv_settings = generateQR();
 
   canvas.style.display = 'block';
   video.style.display = 'none';
 
-  wsRecv(recv_settings, (data) => {
-    video.style.display = 'none';
-    canvas.style.display = 'none';
-    document.getElementById('sendData').style.display = 'none';
-
-    let msg = escapeHTML(data.payload.msg);
-    let outputElem = document.getElementById('payload');
-    outputElem.style.display = 'block';
-    outputElem.innerHTML = `<h2>Received payload:</h2><pre>${msg}</pre>`;
-  });
+  wsRecv(recv_settings, callback);
 }
 
-function sendMessage(payload) {
+function sendMessage(payload, send_settings, callback) {
   canvas.style.display = 'none';
   video.style.display = 'block';
   canvas.style.width = `640px`;
   canvas.style.height = `480px`;
 
-  scanQR((send_settings) => {
-    
-    let context = canvas.getContext('2d');
-    log(video.videoWidth, video.videoHeight);
-    canvas.style.width = `${video.videoWidth}px`;
-    canvas.style.height = `${video.videoHeight}px`;
-        
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    canvas.style.display = 'block';
-    video.style.display = 'none';
-        
-    setTimeout(() => {
-      let outputElem = document.getElementById('payload');
-      outputElem.style.display = 'block';
-      canvas.style.display = 'none';
-      let msg = escapeHTML(payload.msg);
-      outputElem.innerHTML = `<h2>Sent payload:</h2><pre>${msg}</pre>`;
-    }, 1000);
-
-    wsSend(send_settings, payload);
-  });
+  if (send_settings.uuid == null) {
+    // sendMessage called without a UUID, open up the QR code scanner
+    scanQR((qr_data) => {
+      let context = canvas.getContext('2d');
+      log(video.videoWidth, video.videoHeight);
+      canvas.style.width = `${video.videoWidth}px`;
+      canvas.style.height = `${video.videoHeight}px`;
+          
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      canvas.style.display = 'block';
+      video.style.display = 'none';
+          
+      let qr_url = new URL(qr_data);
+      if (qr_url.protocol == location.protocol && qr_url.host == location.host) {
+        // QR url points to the same host, send the data via a web socket
+        wsSend(JSON.parse(decodeURIComponent(qr_url.hash.slice(1))), JSON.stringify(payload));
+        setTimeout(() => callback(payload), 1000);
+      }
+      else {
+        // QR url points to a different host, send the data via a redirect to the specified host
+        redirect(qr_url, payload);
+      }
+    });
+  }
+  else {
+    // sendMessage called with a UUID, send the message directly
+    wsSend(send_settings, JSON.stringify(payload));
+    callback(payload);
+  }
+  
 }
+
+function display_send(payload) {
+  let outputElem = document.getElementById('payload');
+  outputElem.style.display = 'block';
+  canvas.style.display = 'none';
+  let msg = escapeHTML(payload.msg);
+  outputElem.innerHTML = `<h2>Sent payload:</h2><pre>${msg}</pre>`;
+}
+
+function display_recv(payload) {
+  video.style.display = 'none';
+  canvas.style.display = 'none';
+  document.getElementById('sendData').style.display = 'none';
+
+  let msg = escapeHTML(payload.msg);
+  let outputElem = document.getElementById('payload');
+  outputElem.style.display = 'block';
+  outputElem.innerHTML = `<h2>Received payload:</h2><pre>${msg}</pre>`;
+}
+
+let send_settings = {};
+(() => {
+  try {
+    send_settings = JSON.parse(decodeURIComponent(location.hash.slice(1)) || '{}');
+    // Hide the URL data after it has been processed
+    history.replaceState("", document.title, location.pathname + location.search);
+  }
+  catch {}
+
+  log({send_settings});
+
+  if (send_settings.uuid == null) {
+    recvMessage(display_recv);
+  }
+  else {
+    if (send_settings.payload != null) {
+      wsSend({
+        uuid: send_settings.uuid
+      }, send_settings.payload);
+
+      display_send({msg: "Sent encrypted data"});
+    }
+  }
+})();
+
 
 document.getElementById('sendClipboard').onclick = () => {
   document.getElementById('sendClipboard').disabled = true;
@@ -188,7 +247,7 @@ document.getElementById('sendClipboard').onclick = () => {
   navigator.clipboard.readText().then(text => {
     sendMessage({
       msg : text
-    });
+    }, send_settings, display_send);
   });
 }
 
@@ -197,8 +256,6 @@ document.getElementById('sendText').onclick = () => {
   document.getElementById('sendText').disabled = true;
   sendMessage({
     msg : document.getElementById('msg').value || 'Hello World!'
-  });
+  }, send_settings, display_send);
   document.getElementById('msg').value = '';
 }
-
-recvMessage();
